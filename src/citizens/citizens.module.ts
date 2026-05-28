f4ff619634262774ff8923dffff1404f9cdb1bd5
Maj-Britt Deigaard <deigaardm@gmail.com>
2026-05-28 10:37:05 +0200

    ..

--- src/citizens/citizens.module.ts
+++ src/citizens/citizens.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CitizensController } from './citizens.controller';
 import { CitizensService } from './citizens.service';
+import { FileStorageModule } from '../file-storage/file-storage.module';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
+  imports: [FileStorageModule, RedisModule],
   controllers: [CitizensController],
   providers: [CitizensService],
 })

0a5ccff07ee59befe6d9ab229366ea6c09994c7b
Maj-Britt Deigaard <deigaardm@gmail.com>
2026-05-28 10:00:17 +0200

    updated citizen.service to handle data through file storage and cache

--- src/citizens/citizens.service.ts
+++ src/citizens/citizens.service.ts
@@ -1,24 +1,43 @@
+// Handles citizen data through file storage and Redis cache.
+
 import { Injectable } from '@nestjs/common';
+import { FileStorageService } from '../file-storage/file-storage.service';
+import { RedisService } from '../redis/redis.service';
 
 @Injectable()
 export class CitizensService {
-  findAll() {
-    return [
-      {
-        id: '1',
-        fullName: 'Anna Jensen',
-        address: 'Hovedgaden 12',
-        phoneNumber: '12345678',
-      },
-      {
-        id: '2',
-        fullName: 'Peter Hansen',
-        address: 'Søndergade 34',
-        phoneNumber: '87654321',
-      },
-    ];
+  constructor(
+    private readonly fileStorageService: FileStorageService,
+    private readonly redisService: RedisService,
+  ) {}
+
+  // Returns all citizens.
+  // First checks Redis cache. If cache is empty, it reads from JSON file.
+  async findAll() {
+    const cachedCitizens = await this.redisService.getCachedCitizens();
+
+    if (cachedCitizens) {
+      console.log('Citizens loaded from Redis cache');
+
+      return cachedCitizens;
+    }
+
+    console.log('Citizens loaded from JSON file');
+
+    const citizens = await this.fileStorageService.getCitizens();
+
+    await this.redisService.cacheCitizens(citizens);
+
+    return citizens;
   }
 
+  // Returns measurements for one citizen from the JSON file.
+  async findMeasurements(citizenId: string) {
+    return this.fileStorageService.getMeasurementsByCitizenId(citizenId);
+  }
+}
+
+/*
   // TODO 12 det er okay med dummy dataen i den anden metode, men her skal vi maaske have noget database kald ind?
   // Vi kan starte med nogle databasekald som skriver maalinger TIL databasen, eller simpelthen skrive de noedvendige maalinger ind i main.ts
   // Jeg haelder mest til mulighed 2!
@@ -37,4 +56,4 @@ export class CitizensService {
       },
     ];
   }
-}
+}*/
